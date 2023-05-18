ddf666684cb220416476b092d90ae79c5c3677f6
m <maca2002internicola@gmail.com>
2023-05-17 21:32:19 -0300

    hola

---
+++ Entidades/RespuestaPosible.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI_RosaMosqueta.Entidades
+{
+    public class RespuestaPosible
+    {
+        private string Descripcion;
+        private int Valor;
+
+        public RespuestaPosible(string desc, string val)
+        {
+            Descripcion = desc;
+            Valor = val;
+
+        }
+        public string Descrip
+        {
+            get => Descripcion; set => Descripcion = value;
+
+        }
+        public int Valr
+        { get => Valor; set => Valor = value; }
+    }
+}
