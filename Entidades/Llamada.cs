2316e7678f06175ec0a175126b419cc7f16a1a6f
EmilioGalimberti <emiliogalimberty@gmail.com>
2023-05-22 21:20:08 -0300

    +LLamada metodo esDePeriodo() determinarEstadoInicial()

--- Entidades/Llamada.cs
+++ Entidades/Llamada.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PPAI_RosaMosqueta.Entidades
 {
-    internal class Llamada
+    public class Llamada
     {
+        private DateTime fechaActual;
+
+        private List<CambioEstado> cambioEstado { get; set; }
+
+
+        public Llamada(List<CambioEstado> cambioEstado)
+        {
+            this.cambioEstado = cambioEstado;
+        }
+
+        
+        
+        //El método EsDePeriodo() utiliza el método DeterminarEstadoInicial() para obtener la fecha inicial y luego verifica si está dentro del rango definido
+        //por fechaInicio y fechaFin.
+        public bool esDePerido(DateTime  fechaInicio, DateTime  fechaFin)
+        {
+            DateTime fechaHorainicio = determinarEstadoInical();
+            return (fechaHorainicio >= fechaInicio && fechaHorainicio <= fechaFin);
+        }
+
+        //El método DeterminarEstadoInicial() crea una lista de fechas cambiosEstado utilizando el método Select() para obtener
+        //las fechas de inicio de cada elemento de CambioEstado. Luego, se utiliza el método Min() para obtener la fecha mínima de la lista.
+        public DateTime determinarEstadoInical()
+        {
+            List<DateTime> fechasCreacion = this.cambioEstado.Select(fechaHoraInicio => fechaHoraInicio.getFechaHoraIncio()).ToList();
+            return fechasCreacion.Min();
+        }
+
+        //
+
     }
+
 }
