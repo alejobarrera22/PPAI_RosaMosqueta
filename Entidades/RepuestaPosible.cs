6b5148160d2808424beb8c305c214b2e5ebb0116
EmilioGalimberti <emiliogalimberty@gmail.com>
2023-05-24 11:34:34 -0300

    +RepuestaPosible

---
+++ Entidades/RepuestaPosible.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI_RosaMosqueta.Entidades
+{
+    public class RepuestaPosible
+    {
+        private string descripcion { get; set; }
+        //No estoy seguro de que tipo tendria que ser el valor y como utilizarlo
+        private bool valor { get; set; }
+
+        public string getDrescripcionRta()
+        {
+            return descripcion;
+        }
+
+        
+    }
+}
