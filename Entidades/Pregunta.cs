30a839757cca70443435294386a95ac0ee03543b
EmilioGalimberti <emiliogalimberty@gmail.com>
2023-05-27 17:29:31 -0300

    +Pregunta

---
+++ Entidades/Pregunta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI_RosaMosqueta.Entidades
+{
+    public class Pregunta
+    {
+        private string pregunta { get; set; }
+        private List<RepuestaPosible> repuestas { get; set; }
+
+
+        //constructor
+        public Pregunta(string pregunta, List<RepuestaPosible> repuestas)
+        {
+            this.pregunta = pregunta;
+            this.repuestas = repuestas;
+        }
+
+        //faltan los metodos// 
+    }
+}
